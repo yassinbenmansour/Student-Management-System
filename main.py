0a970c0f05df90d0d17937461f2e080297b15791
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-20 23:02:31 +0100

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+#import mysql.connector as mysql
+#from tkinter import ttk,messagebox
+
+page = Tk()
+page.geometry("800x800")
+
+titre = Label(page,text="Gestion stock by Yassine benmansour", font=('Time New Roman',30 , 'bold'))
+titre.pack()
+
+page.mainloop()
